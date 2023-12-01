755ee1d422d728084ecc259c926145edb58a13b6
alatos <latosadam1@gmail.com>
2023-12-01 06:15:48 +0100

    day 1 update test

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -66,7 +66,11 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        let inp = "1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet";
+        let result = part_one(inp);
         assert_eq!(result, Some(142));
     }
 
