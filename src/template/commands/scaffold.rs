c28e1c54eeba78ae8566317d50d62dca00a1a873
Adam Latos <latosadam1@gmail.com>
2023-12-06 17:38:08 +0100

    Modify scaffold

--- src/template/commands/scaffold.rs
+++ src/template/commands/scaffold.rs
@@ -20,15 +20,17 @@ pub fn part_two(input: &str) -> Option<u32> {
 mod tests {
     use super::*;
 
+    const INPUT: &'static str = "";
+
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_one(INPUT);
         assert_eq!(result, None);
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_two(INPUT);
         assert_eq!(result, None);
     }
 }
